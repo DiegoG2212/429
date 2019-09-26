b6b620422fab36e7f8dc4f1c66869e9163d8ad6d
DiegoG2212 <42857014+DiegoG2212@users.noreply.github.com>
2019-09-26 10:37:16 -0700

    Update BetterTokenProcessor.java

--- src/cecs429/text/BetterTokenProcessor.java
+++ src/cecs429/text/BetterTokenProcessor.java
@@ -12,16 +12,16 @@ public class BetterTokenProcessor implements TokenProcessor {
 		
 		token = token.replaceAll("^[^a-zA-Z0-9\\s]+|[^a-zA-Z0-9\\s]+$", ""); // this removes any non alphanumeric values at the beginning or end
 		token = token.replaceAll("\'+|\"", "");	//this removes " and ' from anywhere in the string
-		token = token.replaceAll("\\W", "").toLowerCase();//removes whitespace
 		
-		if (token.contains("\\-")) {
-			String b[] = token.split("\\-");
-			a.add(token.replaceAll("\\-",""));
+		
+		if (token.contains("-")) {
+			String b[] = token.split("-");
+			a.add(token.replaceAll("-",""));
 			for (String i : b) {
 				System.out.println(i);
-				a.add(i);
+				a.add(i.toLowerCase());
 			}
-		}else { a.add(token);}
+		}else { a.add(token.toLowerCase());}
 		return a;
 	}
 
