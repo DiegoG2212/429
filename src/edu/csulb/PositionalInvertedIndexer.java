ed844433e7a79a00d9838f1057f42544389d9b68
MarcM1250 <36463798+MarcM1250@users.noreply.github.com>
2019-10-05 12:46:33 -0700

    Stemming

--- src/edu/csulb/PositionalInvertedIndexer.java
+++ src/edu/csulb/PositionalInvertedIndexer.java
@@ -1,5 +1,6 @@
 package edu.csulb;
 
+// Class Imports
 import cecs429.documents.DirectoryCorpus;
 import cecs429.documents.Document;
 import cecs429.documents.DocumentCorpus;
@@ -10,11 +11,9 @@ import cecs429.index.PositionalInvertedIndex;
 import cecs429.text.BasicTokenProcessor;
 import cecs429.text.BetterTokenProcessor;
 import cecs429.text.EnglishTokenStream;
+import org.tartarus.snowball.ext.englishStemmer;
 
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+// General Imports
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -25,7 +24,9 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
 
+// GUI Imports
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -34,13 +35,17 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class PositionalInvertedIndexer {
 	String directory = ""; // Sets directory to blank
 	File defStore = new File("src/DefaultDirectory.txt"); // Text file storing Default Directory
-	DocumentCorpus corpus = DirectoryCorpus.loadJsonDirectory(Paths.get(directory).toAbsolutePath(), ".json");
-	//static DocumentCorpus corpus = DirectoryCorpus.loadTextDirectory(Paths.get("C:\\Users\\potad\\eclipse-workspace\\Text Files").toAbsolutePath(), ".txt");
+	//DocumentCorpus corpus = DirectoryCorpus.loadJsonDirectory(Paths.get(directory).toAbsolutePath(), ".json");
+	DocumentCorpus corpus = DirectoryCorpus.loadTextDirectory(Paths.get("C:\\Users\\potad\\eclipse-workspace\\Text Files").toAbsolutePath(), ".txt");
 	Index index = indexCorpus(corpus);
 	
 	public PositionalInvertedIndexer() throws Exception {
@@ -78,16 +83,19 @@ public class PositionalInvertedIndexer {
 				JButton browseFile = new JButton("Browse Files");
 				JLabel l = new JLabel("Blank");
 				JTextArea results = new JTextArea(19,55);
+				//JTextPane results = new JTextPane("text/html", "");
 				JScrollPane scrollPane = new JScrollPane(results);		
 				JFileChooser j = new JFileChooser();
 				j.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 				
 				
+				// Fonts
 				Font font = new Font(Font.SANS_SERIF, Font.BOLD, 15);
+				Font bold = new Font(Font.SANS_SERIF, Font.BOLD, 15);
 				Font inputFont = new Font(Font.SANS_SERIF, Font.PLAIN, 18);
 				Font resultFont = new Font(Font.SANS_SERIF, Font.PLAIN, 17);
 				
-				// Browse Files Action Listener
+				// Browse Files Action Listener; :index Special Query
 				browseFile.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 
@@ -108,19 +116,29 @@ public class PositionalInvertedIndexer {
 							}			
 					}				
 				});		
-
-				
+		
 				// Search Button Action Listener
 				search.addActionListener(new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						String query = textField.getText().toLowerCase();
-						//query = query.toLowerCase();					
+						englishStemmer stemmer = new englishStemmer();
+						String query = textField.getText().toLowerCase();	
+						
+						// Stem Queries
+						stemmer.setCurrent(query);
+						stemmer.stem();
+						query = stemmer.getCurrent();
+						System.out.println(query);
+						
 						results.setText(""); // Clear results
 						
+						// Gets first word in query
+						String special[] = textField.getText().split(" ", 2);
+						
+						// :vocab Special Query
 						if(textField.getText().equals(":vocab")) {
 							List<String> t = PositionalInvertedIndexer.this.index.getVocabulary();
-							
+				
 							int counter = 0;
 							for (String i : t) {
 								results.append(i +"\n");
@@ -128,25 +146,43 @@ public class PositionalInvertedIndexer {
 								if(counter >= 1000) //Stops after first 1000 terms
 									break;
 							}
-							results.append("woah \n");
+							results.append("End of Vocabulary \n");	
+						}
+						
+						// :stem token Special Query
+						else if(special[0].equals(":stem")){
+							try {
+									stemmer.setCurrent(special[1]);
+									if(stemmer.stem()) {
+										results.append(stemmer.getCurrent() + "\n");
+									}
+							}
+							catch(ArrayIndexOutOfBoundsException exception) {
+								results.setText("Correct usage: \":stem <token>\" ");
+							}
+							
 						}
 						
 						
-						int docCount = 0;
-						for (Posting p : PositionalInvertedIndexer.this.index.getPostings(query)) {
-							results.append("Document: " + PositionalInvertedIndexer.this.corpus.getDocument(p.getDocumentId()).getTitle() +"\n");
-							results.append("Positions: " + p.getPos() +"\n");
-							docCount++;
-						}
-						results.append("Number of Documents:" + docCount +"\n");
-						results.append("\n");
+						else {
+							int docCount = 0;
+							for (Posting p : PositionalInvertedIndexer.this.index.getPostings(query)) {
+								results.append("Document: " + PositionalInvertedIndexer.this.corpus.getDocument(p.getDocumentId()).getTitle() +"\n");
+								results.append("Positions: " + p.getPos() +"\n");
+								docCount++;
+							}
+							results.append("Number of Documents:" + docCount +"\n");
+							results.append( "\n");
+						}	
+						
+						
+						
 						}					
 					
 				});
 				
 				results.setEditable(false);
 				
-	
 				// Panel Add	
 				p.add(browseFile);
 				p.add(textField);
@@ -171,29 +207,27 @@ public class PositionalInvertedIndexer {
 				textField.setFont(inputFont);
 				results.setFont(resultFont);
 				
-				frame.setResizable(false);	
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Closing window stops program
-				frame.setVisible(true);
+				frame.setResizable(false);	// No window resizing
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Closing window stops program; :q Special Query
+				frame.setVisible(true); // Visible
 			}
 		});
-		// GUI End===================================================================================
-		
-
-		
-		
+		// GUI End===================================================================================	
 	}
 	
 	public void updateDirectory(String dir) { //Updates changes to corpus and index
-		PositionalInvertedIndexer.this.corpus = DirectoryCorpus.loadJsonDirectory(Paths.get(dir).toAbsolutePath(), ".json");
+		//PositionalInvertedIndexer.this.corpus = DirectoryCorpus.loadJsonDirectory(Paths.get(dir).toAbsolutePath(), ".json");
+		PositionalInvertedIndexer.this.corpus = DirectoryCorpus.loadTextDirectory(Paths.get(dir).toAbsolutePath(), ".txt");
 		PositionalInvertedIndexer.this.index = indexCorpus(PositionalInvertedIndexer.this.corpus);
 	}	
+	
 	private Index indexCorpus(DocumentCorpus corpus) {
 		BetterTokenProcessor processor = new BetterTokenProcessor();
 		PositionalInvertedIndex tdi = new PositionalInvertedIndex();
 	
 		// Loops through documents
 		for (Document d : corpus.getDocuments()) {
-			int x = 0; //Reset counter
+			int x = 0; //Reset counter for positions
 			// Creates tokens by splitting on whitespace
 			EnglishTokenStream stream = new EnglishTokenStream(d.getContent());
 			
@@ -215,10 +249,8 @@ public class PositionalInvertedIndexer {
 	}
 	
 	
-	
-	
 	public static void main(String[] args) throws Exception {
-		new PositionalInvertedIndexer();
+		new PositionalInvertedIndexer(); // Calls program
 	}
 	
 }
