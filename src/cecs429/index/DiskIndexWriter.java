f19cdc09e4e720aab8783b56e2239bb60d02cb64
MarcM1250 <36463798+MarcM1250@users.noreply.github.com>
2019-10-16 14:38:53 -0700

    Vocab.bin Finished

--- src/cecs429/index/DiskIndexWriter.java
+++ src/cecs429/index/DiskIndexWriter.java
@@ -12,23 +12,13 @@ import java.util.List;
 import edu.csulb.PositionalInvertedIndexer;
 
 public class DiskIndexWriter {
-	
 	Index index;	// Index
 	Path path; 		//  points to the directory for saving the three index files
-	public void WriteIndex(Index x, Path y) throws IOException {
-		index = x;
-		path = y;
-		
+	public void WriteIndex(Index x, Path y) throws IOException {		
 		// Sub Methods
-		//writePostings();
 		writeVocab();
-		//writeVocabTable();
-	}
-	
-	// postings.bin
-	public void writePostings() throws IOException {
-		DataOutputStream postingsOut = new DataOutputStream(new FileOutputStream(path + ""));
-		
+		//writePostings();
+		//writeVocabTable();	
 	}
 	
 	// vocab.bin encoded using UTF-8
@@ -39,17 +29,26 @@ public class DiskIndexWriter {
 				new BufferedOutputStream(
 				new FileOutputStream(path + "/vocab.bin")));
 		
-		List<String> t = index.getVocabulary();
+		List<String> t = index.getVocabulary();	// Get vocab from index
+		
 		for (String i : t) {	// Go through vocabulary, write to vocab.bin
-			vocabOut.writeUTF(i);
+			vocabOut.writeUTF(i);	// UTF-8 Encoded
 		}
 		vocabOut.close();
+	}
+		
+	// postings.bin
+	public void writePostings() throws IOException {
+		DataOutputStream postingsOut = new DataOutputStream(
+				new BufferedOutputStream(
+				new FileOutputStream(path + "/postings.bin")));	
 	}
 	
 	// vocabTable.bin
 	public void writeVocabTable() throws FileNotFoundException{
-		DataOutputStream vtableOut = new DataOutputStream(new FileOutputStream(path + ""));
-		
+		DataOutputStream vtableOut = new DataOutputStream(
+				new BufferedOutputStream(
+				new FileOutputStream(path + "/vocabTable.bin")));
 	}
 
 }
