d8052a075a06d1b10496bba2f6f5fab0854d2712
DiegoG2212 <42857014+DiegoG2212@users.noreply.github.com>
2019-10-16 23:04:31 -0700

    finish disk writer hoping its gud

--- src/cecs429/index/DiskIndexWriter.java
+++ src/cecs429/index/DiskIndexWriter.java
@@ -7,32 +7,35 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.csulb.PositionalInvertedIndexer;
 
 public class DiskIndexWriter {
-	Index index;	// Index
-	Path path; 		//  points to the directory for saving the three index files
+
 	public void WriteIndex(Index x, Path y) throws IOException {
-		index = x;
-		path = y;
+
 		// Sub Methods
-		
-		writePostings();
-		writeVocab();
-		//writeVocabTable();	
+
+		writeVocabTable(y,x);
 	}
 	
 	// postings.bin
-	void writePostings() throws IOException {
+	private List<Long> writePostings(Path path, Index index) throws IOException {
 		System.out.println("Writing postings.bin ...");
+		List<Long> docPos = new ArrayList<>();
+
 		DataOutputStream postingsOut = new DataOutputStream(
 				new BufferedOutputStream(
 				new FileOutputStream(path + "/postings.bin")));	
 		
 		List<String> t = index.getVocabulary();
-		for (String i : t) {	// Go through vocabulary
+		long position = 0;
+		for (String i : t) {// Go through vocabulary
+
+			position = postingsOut.size() - position;
+			docPos.add(position);
 			for (Posting p : index.getPostings(i)) { // Get posting for term
 				postingsOut.writeInt(p.getDocumentId());
 				//System.out.println(p.getDocumentId());
@@ -43,49 +46,62 @@ public class DiskIndexWriter {
 			}	
 		}
 		postingsOut.close();
+
+		return docPos;
 	}
 	
 	// vocab.bin encoded using UTF-8
-	public void writeVocab() throws IOException{
+	private List<Long> writeVocab(Path path, Index index) throws IOException{
 		System.out.println("Writing vocab.bin ...");
-		
+		List<Long> vocabPos = new ArrayList<>();
 		DataOutputStream vocabOut = new DataOutputStream(
 				new BufferedOutputStream(
 				new FileOutputStream(path + "/vocab.bin")));
 		
 		List<String> t = index.getVocabulary();	// Get vocab from index
-		
-		int counter = 0;
+		long position = 0;
+
 		for (String i : t) {	// Go through vocabulary, write to vocab.bin
-			long position = vocabOut.size();
-			
+
+			position =  vocabOut.size() - position;
+			vocabPos.add(position);
 			System.out.println(position);
 			System.out.println("Vocab: "+ i);
 			vocabOut.writeUTF(i);	// UTF-8 Encoded
-			counter++;
-			if(counter == 10) { // Stops at 10 terms
-				position = vocabOut.size();
-				System.out.println(position);
-				break;
-			}
-			
 			
 		}
 		vocabOut.close();
+
+		return vocabPos;
 	}
 		
 
 	
 	// vocabTable.bin
-	public void writeVocabTable() throws FileNotFoundException{
+	private void writeVocabTable(Path path, Index index) throws IOException{
 		System.out.println("Writing vocabTable.bin ...");
+
+
+
+		List<Long> docPos = writePostings(path, index);
+		List<Long> vocabPos = writeVocab(path, index);
+
+
 		DataOutputStream vtableOut = new DataOutputStream(
 				new BufferedOutputStream(
-				new FileOutputStream(path + "/vocabTable.bin")));
-		
-		
-		
-		
+						new FileOutputStream(path + "/vocabTable.bin")));
+
+
+		for (int i = 0; i <docPos.size(); i++){
+
+			long vPos = vocabPos.get(i);
+			long dPos = docPos.get(i);
+
+			vtableOut.writeLong(vPos);
+			vtableOut.writeLong(dPos);
+			}
+
+		vtableOut.close();
 		
 	}
 
