e5c9cbecd7d6f4997aa2430842c77461e4af4008
Ky-Vu <54649640+Ky-Vu@users.noreply.github.com>
2019-11-03 22:44:37 -0800

    Updated most method functionalities
    Still need to do readDocWeights

--- src/cecs429/index/DiskPositionalIndex.java
+++ src/cecs429/index/DiskPositionalIndex.java
@@ -1,38 +1,47 @@
 package cecs429.index;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
 import org.mapdb.btree.*;
 public class DiskPositionalIndex implements Index {
-	
-	private HashMap<String, ArrayList<Posting>> map;
+
+	private BTreeMap<String, ArrayList<Posting>> T;
 	
 	// Maps docIDs to terms appearing in that doc
 	private HashMap<Integer, HashMap<String, Integer>> docTerms;
-	private BTreeMap<String, Posting> T;
 	@Override
 	public List<Posting> getPostings(String term) {
-		// TODO Auto-generated method stub
-		return null;
+		if (T.containsKey(term)) {
+			return T.get(term);
+		} else {
+			List<Posting> p = Collections.emptyList();
+			return p;
+		}
 	}
 
 	@Override
 	public List<String> getVocabulary() {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> vocab = new ArrayList<String>();
+		for (Map.Entry<String, ArrayList<Posting>> entry : T.entrySet()) {
+			vocab.add(entry.getKey());
+		}
+		return vocab;
 	}
 
 	@Override
 	public void addTerm(List<String> term, int docID, int pos) {
 		for (String i : term) {
-			if (!map.containsKey(i)) { // If map doesn't contain term
+			if (!T.containsKey(i)) { // If map doesn't contain term
 				Posting p = new Posting(docID, pos); // Add term to map
 				ArrayList<Posting> l = new ArrayList<Posting>();
 				l.add(p);
-				map.put(i, l);
+				T.put(i, l);
 			} else {
-				List<Posting> temp = map.get(i);
+				List<Posting> temp = T.get(i);
 
 				if (docID == temp.get(temp.size() - 1).getDocumentId()) {
 					temp.get(temp.size() - 1).addPos(pos);
@@ -40,7 +49,7 @@ public class DiskPositionalIndex implements Index {
 				} else {
 					Posting p = new Posting(docID, pos);
 					temp.add(p);
-					map.put(i, new ArrayList<>(temp));
+					T.put(i, new ArrayList<>(temp));
 				}
 			}
 		}
@@ -57,8 +66,16 @@ public class DiskPositionalIndex implements Index {
 	
 	// Reads docWeights.bin to skip to an appropriate location
 	// to read an 8-byte double for Ld
-	public void readDocWeights() {
-		
+	public void readDocWeights(String binLocation) {
+		try (InputStream in = new FileInputStream(binLocation);) {
+			int byteRead;
+
+			while ((byteRead = in.read()) != -1) {
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
