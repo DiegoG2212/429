fbef1dc9a74db7f59549f16c1d29776d444d2d31
Ky-Vu <54649640+Ky-Vu@users.noreply.github.com>
2019-11-07 12:00:46 -0800

    Added strategy design for formulas

---
+++ src/cecs429/rankings/OkapiRank.java
@@ -0,0 +1,20 @@
+package cecs429.rankings;
+
+import cecs429.index.Index;
+
+class OkapiRank implements RankFormula {
+    @Override
+    public double getWqt(Index i, String term) {
+        return 0;
+    }
+
+    @Override
+    public double getWdt(Index i, String term, int docID) {
+        return 0;
+    }
+
+    @Override
+    public double getLd(int docID) {
+        return 0;
+    }
+}
