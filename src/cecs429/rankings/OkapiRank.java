ea12089ed257835cefba42dc0649d03c8ded9754
DiegoG2212 <42857014+DiegoG2212@users.noreply.github.com>
2019-11-13 15:01:22 -0800

    Update OkapiRank.java

--- src/cecs429/rankings/OkapiRank.java
+++ src/cecs429/rankings/OkapiRank.java
@@ -10,13 +10,23 @@ public class OkapiRank implements RankFormula {
         return 0;
     }
 
-    @Override
+<<<<<<< HEAD
+
     public double getWdt(int x) {
+=======
+    @Override
+    public double getWdt() {
+>>>>>>> f4198b343b068328156a4cf8344a6543400da62a
+        return 0;
+    }
+
+    @Override
+    public double getWdt() {
         return 0;
     }
 
     @Override
     public double getLd() {
-        return 0;
+        return 1;
     }
 }
