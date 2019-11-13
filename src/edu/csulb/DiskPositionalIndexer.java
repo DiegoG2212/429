8470591de08f441475e4d0ed2933f8e8785097db
Ky-Vu <54649640+Ky-Vu@users.noreply.github.com>
2019-11-12 22:12:47 -0800

    Updated DiskPositionalIndexer for ranking calculations

--- src/edu/csulb/DiskPositionalIndexer.java
+++ src/edu/csulb/DiskPositionalIndexer.java
@@ -1,41 +1,34 @@
 package edu.csulb;
 
 // Class Imports
+
+import cecs429.disk.DiskIndexWriter;
 import cecs429.disk.DiskInvertedIndex;
 import cecs429.documents.DirectoryCorpus;
 import cecs429.documents.Document;
 import cecs429.documents.DocumentCorpus;
-import cecs429.disk.DiskIndexWriter;
-import cecs429.index.DiskPositionalIndex;
 import cecs429.index.Index;
 import cecs429.index.Posting;
 import cecs429.query.BooleanQueryParser;
 import cecs429.query.QueryComponent;
 import cecs429.index.PositionalInvertedIndex;
+import cecs429.rankings.DefaultRank;
+import cecs429.rankings.RankFormula;
 import cecs429.text.BetterTokenProcessor;
 import cecs429.text.EnglishTokenStream;
 import org.tartarus.snowball.ext.englishStemmer;
 
 // General Imports
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 // GUI Imports
 import javax.swing.JButton;
-import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -49,23 +42,27 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DiskPositionalIndexer {
-	String directory = ""; // Sets directory to blank
-	// File defStore = new File("src/DefaultDirectory.txt"); // Text file storing
-	// Default Directory
-	// DocumentCorpus corpus = DirectoryCorpus.loadJsonDirectory(Paths.get(directory).toAbsolutePath(), ".json");
-	DocumentCorpus corpus = DirectoryCorpus.loadTextDirectory(Paths.get(directory).toAbsolutePath(),".txt");
-	String lastQuery = ""; // Saves last user query
-	int queryCheck = 0;
-	Index index;
+    String directory = ""; // Sets directory to blank
+    // File defStore = new File("src/DefaultDirectory.txt"); // Text file storing
+    // Default Directory
+    // DocumentCorpus corpus = DirectoryCorpus.loadJsonDirectory(Paths.get(directory).toAbsolutePath(), ".json");
+    DocumentCorpus corpus = DirectoryCorpus.loadTextDirectory(Paths.get(directory).toAbsolutePath(), ".txt");
+    String lastQuery = ""; // Saves last user query
+    int queryCheck = 0;
+    Index index;
 
-	long indexTime = 0;
+    long indexTime = 0;
 
-	public DiskPositionalIndexer() throws Exception {
-		query();
-	}
+    RankFormula rf;
+    //
+
+    public DiskPositionalIndexer() throws Exception {
+        query();
+    }
 
 	public void query() throws Exception {
 		// Load Default Directory (Last selected folder)
@@ -106,9 +103,9 @@ public class DiskPositionalIndexer {
 				results.setLineWrap(true);
 				results.setWrapStyleWord(true);
 
-				
-				
-				
+
+
+
 				// Browse Files Action Listener; :index Special Query
 				browseFile.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
@@ -222,12 +219,12 @@ public class DiskPositionalIndexer {
 								 * { String compare =
 								 * PositionalInvertedIndexer.this.corpus.getDocument(p.getDocumentId()).getTitle
 								 * ().toLowerCase();
-								 * 
+								 *
 								 * compare = stemThis(compare); if(docSearch.equals(compare)) {
 								 * results.append("\n"); Reader b =
 								 * PositionalInvertedIndexer.this.corpus.getDocument(p.getDocumentId()).
 								 * getContent(); String read = "";
-								 * 
+								 *
 								 * int c = 0; try { c = b.read(); } catch (IOException e1) {
 								 * e1.printStackTrace(); } while (c != -1){ //Converting to character
 								 * //System.out.print((char)c); read += Character.toString((char)c); try { c =
@@ -280,36 +277,36 @@ public class DiskPositionalIndexer {
 
 				});
 
-				
-				
-				
-				
+
+
+
+
 				results.setEditable(false); // Doesn't let user edit results box
-				
+
 				// Opening Dialog Box to select Mode
 				Object[] options = {"Boolean query mode",
-				                    "Ranked query mode",
-				                    "None"};
+						"Ranked query mode",
+						"None"};
 				int n = JOptionPane.showOptionDialog(frame,
-				    "What mode would you like to use?",
-				    "Mode Selection",
-				    JOptionPane.YES_NO_CANCEL_OPTION,
-				    JOptionPane.QUESTION_MESSAGE,
-				    null,
-				    options,
-				    options[2]);
+						"What mode would you like to use?",
+						"Mode Selection",
+						JOptionPane.YES_NO_CANCEL_OPTION,
+						JOptionPane.QUESTION_MESSAGE,
+						null,
+						options,
+						options[2]);
 				// System.out.println(n);
 				if(n == 0) {	//Boolean query mode
-					
+
 				}
 				else if(n == 1) {	//Ranked query mode
-					
+
 				}
 				else {
 					// End Program
 					System.exit(-1);
 				}
-				
+
 				// Panel Add
 				p.add(browseFile);
 				p.add(textField);
@@ -337,121 +334,185 @@ public class DiskPositionalIndexer {
 		// End===================================================================================
 	}
 
-	public void updateDirectory(String dir) throws IOException { // Updates changes to corpus and index
-		// PositionalInvertedIndexer.this.corpus = DirectoryCorpus.loadJsonDirectory(Paths.get(dir).toAbsolutePath(),".json");
-		DiskPositionalIndexer.this.corpus = DirectoryCorpus.loadTextDirectory(Paths.get(dir).toAbsolutePath(), ".txt");
+    //
+    public void updateDirectory(String dir) throws IOException { // Updates changes to corpus and index
+        // PositionalInvertedIndexer.this.corpus = DirectoryCorpus.loadJsonDirectory(Paths.get(dir).toAbsolutePath(),".json");
+        DiskPositionalIndexer.this.corpus = DirectoryCorpus.loadTextDirectory(Paths.get(dir).toAbsolutePath(), ".txt");
 
-		long startTime = System.nanoTime(); // Index Timer
-		DiskPositionalIndexer.this.index = indexCorpus(DiskPositionalIndexer.this.corpus);
-		long endTime = System.nanoTime();
+        long startTime = System.nanoTime(); // Index Timer
+        DiskPositionalIndexer.this.index = indexCorpus(DiskPositionalIndexer.this.corpus);
+        long endTime = System.nanoTime();
 
-		DiskPositionalIndexer.this.indexTime = (endTime - startTime) / 1000000;
-		System.out.println(indexTime + " milliseconds");
-	}
+        DiskPositionalIndexer.this.indexTime = (endTime - startTime) / 1000000;
+        System.out.println(indexTime + " milliseconds");
+    }
 
-	public String stemThis(String x) { // Updates changes to corpus and index
-		englishStemmer stemmer = new englishStemmer();
+    public String stemThis(String x) { // Updates changes to corpus and index
+        englishStemmer stemmer = new englishStemmer();
 
-		// Stem Query
-		stemmer.setCurrent(x);
-		stemmer.stem();
-		return stemmer.getCurrent();
-	}
+        // Stem Query
+        stemmer.setCurrent(x);
+        stemmer.stem();
+        return stemmer.getCurrent();
+    }
 
-	private Index indexCorpus(DocumentCorpus corpus) throws IOException {
-		BetterTokenProcessor processor = new BetterTokenProcessor();
-		//Index tdi = new DiskInvertedIndex(Paths.get(directory +"/index").toAbsolutePath());
-		Index tdi = null;
-		if(Files.exists(Paths.get(directory + "/index/vocabTable.bin").toAbsolutePath()) == false) { // If index folder doesn't exist
-			System.out.println("Writing Index to Disk...");
-			tdi = new PositionalInvertedIndex();
-			DiskIndexWriter writeDisk = new DiskIndexWriter(); // Writes index to disk
-			// Loops through documents
-			for (Document d : corpus.getDocuments()) {
-				int x = 0; // Reset counter for positions
-				// Creates tokens by splitting on whitespace
-				EnglishTokenStream stream = new EnglishTokenStream(d.getContent());
-				List<String> token2 = new ArrayList<String>();
-				HashMap<String, Integer> terms = new HashMap<String, Integer>();
+    private Index indexCorpus(DocumentCorpus corpus) throws IOException {
+        BetterTokenProcessor processor = new BetterTokenProcessor();
+        Index tdi;
+        if (Files.exists(Paths.get(directory + "/index/vocabTable.bin").toAbsolutePath()) == false) { // If index folder doesn't exist
+            System.out.println("Writing Index to Disk...");
+            tdi = new PositionalInvertedIndex();
+            DiskIndexWriter writeDisk = new DiskIndexWriter(); // Writes index to disk
+
+			// Lists for calculating each individual document's weightings
+			List<Double> docWeights = Collections.emptyList(); //Ld of each doc in Default & tfidf
+			List<Double> docLengths = Collections.emptyList();
+			// List to get the average for term frequency of each term in the doc
+			List<Double> dAveTFtd = Collections.emptyList();
+
+            // Loops through documents
+            for (Document d : corpus.getDocuments()) {
+                int x = 0; // Reset counter for positions
+                // Creates tokens by splitting on whitespace
+                EnglishTokenStream stream = new EnglishTokenStream(d.getContent());
+                List<String> token2 = new ArrayList<String>();
+
+                //Hashmap to keep track of the frequency of each term in the doc
+                HashMap<String, Integer> terms = new HashMap<String, Integer>();
+
+				// List of Wdts to calculate the Ld of each doc
+				List<Double> Wdts = Collections.emptyList();
+
+                // Count number of tokens for each doc
+                int tokenCount = 0;
+
+                // Adds term to index along with Document ID
+                for (String token : stream.getTokens()) {    // Go through each token
+                    // Increment token counter each time
+                    tokenCount++;
+                    // Add processed token
+                    tdi.addTerm(processor.processToken(token), d.getId(), x);
+                    //System.out.println(processor.processToken(token));
 
 
-				int termExist = 0;
-				// Adds term to index along with Document ID
-				for (String token : stream.getTokens()) {    // Go through each token
-					// Add processed token
-					tdi.addTerm(processor.processToken(token), d.getId(), x);
-					//System.out.println(processor.processToken(token));
+                    // Doc Weight Work ===================================================
+
+                    for (String i : processor.processToken(token)) {
+                        // If term exists in HashMap
+                        if (terms.containsKey(i)) {
+                            int valHold = terms.get(i); // Take value
+                            valHold++; // Increment counter by 1
+                            terms.put(i, valHold);
+                        } else { // Term doesn't currently exist
+                            terms.put(i, 1); // Give count of 1
+                        }
+                    }
+                    // ====================================================================
 
 
-					// Doc Weight Work ==================================================
-					for (String i : processor.processToken(token)) {
-						// If term exists in HashMap
-						if (terms.containsKey(i)) {
-							int valHold = terms.get(i); // Take value
-							valHold++; // Increment counter by 1
-							terms.put(i, valHold);
-						} else { // Term doesn't currently exist
-							terms.put(i, 1); // Give count of 1
-						}
-					}
-					// ====================================================================
+                    for (String i : processor.processToken(token)) {
+                        token2.add(i);
+                    }
+                    while (token2.size() >= 2) {
+                        String temp = token2.get(0) + " " + token2.get(1);
+                        List<String> token3 = new ArrayList<String>();
+                        token3.add(temp);
+                        tdi.addTerm(token3, d.getId(), x);
+                        // Doc Weight Work ===================================================
+                        for (String b : token3) {
+                            if (terms.containsKey(b)) {
+                                int valHold = terms.get(b); // Take value
+                                valHold++; // Increment counter by 1
+                                terms.put(b, valHold);
+                            } else { // Term doesn't currently exist
+                                terms.put(b, 1); // Give count of 1
+                            }
+                        }
+                        // ====================================================================
+                        token2.remove(0);
+                    }
 
-
-					for (String i : processor.processToken(token)) {
-						token2.add(i);
-					}
-					while (token2.size() >= 2) {
-						String temp = token2.get(0) + " " + token2.get(1);
-						List<String> token3 = new ArrayList<String>();
-						token3.add(temp);
-						tdi.addTerm(token3, d.getId(), x);
-						// Doc Weight Work ===================================================
-						for (String b : token3) {
-							if (terms.containsKey(b)) {
-								int valHold = terms.get(b); // Take value
-								valHold++; // Increment counter by 1
-								terms.put(b, valHold);
-							} else { // Term doesn't currently exist
-								terms.put(b, 1); // Give count of 1
-							}
-						}
-						// ====================================================================
-						token2.remove(0);
-					}
-
-					x++;
+                    x++;
+                }
+                // Get the Wdts for the doc and add it into the list
+				for (Map.Entry<String, Integer> entry : terms.entrySet()) {
+					Wdts.add(this.getWdt(entry.getValue()));
 				}
-
-				//writeDisk.addDocWeight(terms); // Add HashMap to list
-				try {
-					stream.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				double Ldcalc = 0;
+				for (double dub : Wdts) {
+					Ldcalc += Math.pow(dub, 2);
 				}
+				docWeights.add(Math.sqrt(Ldcalc));
 
-			}    // End of Documents
+				//Add doc lengths into list
+                docLengths.add((double)tokenCount);
+                double tftdsum = 0;
+                for (Map.Entry<String, Integer> entry : terms.entrySet()) {
+                    tftdsum += (double) entry.getValue();
+                }
+                tftdsum /= terms.size();
+                dAveTFtd.add(tftdsum);
 
+                //writeDisk.addDocWeight(terms); // Add HashMap to list
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-			// Write to Disk
-			writeDisk.WriteIndex(tdi, Paths.get(directory + "/index").toAbsolutePath());
-		}
+            }    // End of Documents
 
-		else{ // Already on disk
-			System.out.println("Index on Disk");
-			tdi = new DiskInvertedIndex(Paths.get(directory + "/index").toAbsolutePath());
-		}
+			//List to get the byte size of each doc
+			List<Long> dBytes = getByteSizes();
 
+            // Write to Disk
+            writeDisk.WriteIndex(tdi, Paths.get(directory + "/index").toAbsolutePath());
 
-		//System.out.println(tdi.getVocabulary());
+            // Return Index
+            return tdi;
+        } else { // Already on disk
+            System.out.println("Index on Disk");
+            tdi = new DiskInvertedIndex(Paths.get(directory + "/index").toAbsolutePath());
+        }
+        //System.out.println(tdi.getVocabulary());
 
-		System.out.println(tdi.getVocabulary().toString());
-		// Return Index
-		return tdi;
-	}
+        System.out.println(tdi.getVocabulary().toString());
+        // Return Index
+        return tdi;
+    }
 
-	public static void main(String[] args) throws Exception {
-		new DiskPositionalIndexer(); // Calls program
-	}
+    public List<Long> getByteSizes() {
+        List<Long> result = new ArrayList<Long>();
+
+        try (Stream<Path> walk = Files.walk(Paths.get(directory).toAbsolutePath(), 1)) {
+            result = walk.filter(f -> f.getFileName().toString().endsWith(".txt"))
+                    .map(p -> p.toFile().length())
+                    .collect(Collectors.toList());
+
+            for (Long l : result) {
+                System.out.println(l);
+            }
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+
+        return result;
+    }
+
+    private double getWdt(int tftd) {
+        return 1 + Math.log(tftd);
+    }
+
+    private double Ld(List<Double> Wdts) {
+        double docWeight = 0;
+        for (double d : Wdts) {
+            docWeight += Math.pow(d, 2);
+        }
+        return Math.sqrt(docWeight);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new DiskPositionalIndexer(); // Calls program
+    }
 
 }
